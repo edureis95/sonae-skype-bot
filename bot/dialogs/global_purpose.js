3fdf318e986333626e6f9a795156d7992da0ebf6
inessousacaldas <ines.sousacaldas@gmail.com>
2017-03-31 19:55:02 +0100

    Image dialog in correct file. dialog flux with some errors

---
+++ bot/dialogs/global_purpose.js
@@ -0,0 +1,85 @@
+var builder = require('botbuilder');
+var captionService = require('../../caption-service');
+var needle = require('needle');
+
+var lib = new builder.Library('global_purpose');
+
+
+
+lib.dialog('analyze_image', function (session) {
+    
+     var stream = getImageStreamFromMessage(session.message);
+        captionService
+            .getCaptionFromStream(stream)
+            .then(function (caption) { handleSuccessResponse(session, caption); })
+            .catch(function (error) { handleErrorResponse(session, error); });
+
+
+});
+
+//=========================================================
+// Utilities
+//=========================================================
+
+function getImageStreamFromMessage(message) {
+    var headers = {};
+    var attachment = message.attachments[0];
+    if (checkRequiresToken(message)) {
+        // The Skype attachment URLs are secured by JwtToken,
+        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+        // https://github.com/Microsoft/BotBuilder/issues/662
+        connector.getAccessToken(function (error, token) {
+            var tok = token;
+            headers['Authorization'] = 'Bearer ' + token;
+            headers['Content-Type'] = 'application/octet-stream';
+
+            return needle.get(attachment.contentUrl, { headers: headers });
+        });
+    }
+
+    headers['Content-Type'] = attachment.contentType;
+    return needle.get(attachment.contentUrl, { headers: headers });
+}
+
+function checkRequiresToken(message) {
+    return message.source === 'skype' || message.source === 'msteams';
+}
+
+/**
+ * Gets the href value in an anchor element.
+ * Skype transforms raw urls to html. Here we extract the href value from the url
+ * @param {string} input Anchor Tag
+ * @return {string} Url matched or null
+ */
+function parseAnchorTag(input) {
+    var match = input.match('^<a href=\"([^\"]*)\">[^<]*</a>$');
+    if (match && match[1]) {
+        return match[1];
+    }
+
+    return null;
+}
+
+//=========================================================
+// Response Handling
+//=========================================================
+function handleSuccessResponse(session, caption) {
+    if (caption) {
+        session.send('I think it\'s ' + caption);
+    }
+    else {
+        session.send('Couldn\'t find a caption for this one');
+    }
+
+}
+
+function handleErrorResponse(session, error) {
+    session.send('Oops! Something went wrong. Try again later.');
+    console.error(error);
+}
+
+
+// Export createLibrary() function
+module.exports.createLibrary = function () {
+    return lib.clone();
+};
---
+++ bot/dialogs/other.js
@@ -0,0 +1,32 @@
+var builder = require('botbuilder');
+
+var lib = new builder.Library('other');
+
+var HelpMessage = '\n Posso ajudar-te nas seguintes áreas:\n * Reuniões\n * Produtividade\n * Suporte\n * Dicas e Atalhos\n * Propósitos Gerais';
+var UserNameKey = 'UserName';
+var UserWelcomedKey = 'UserWelcomed';
+
+// Greet dialog
+lib.dialog('greet', new builder.SimpleDialog(function (session, results) {
+    if (results && results.response) {
+        session.userData[UserNameKey] = results.response;
+        session.privateConversationData[UserWelcomedKey] = true;
+
+        return session.endDialog('Olá %s! %s', results.response, HelpMessage);
+    }
+
+    builder.Prompts.text(session, 'Olá, como te chamas?');
+}));
+
+// Help dialog
+lib.dialog('help', function (session) {
+    
+    return session.send(HelpMessage);
+
+});
+
+
+// Export createLibrary() function
+module.exports.createLibrary = function () {
+    return lib.clone();
+};
