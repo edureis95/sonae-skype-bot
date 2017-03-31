8453bf24df2dacb4b5395dc32bd86a47e8f931e9
edureis95 <up201303832@gcloud.fe.up.pt>
2017-03-31 16:25:39 +0100

    Bot explica o que é a imagem em Inglês

--- bot/index.js
+++ bot/index.js
@@ -1,3 +1,11 @@
+/*var gcloud = require('gcloud')({
+  keyFilename: 'googleAPIKey/key.json',
+  projectId: 'persuasive-yeti-163120'
+});*/
+
+var captionService = require('../caption-service');
+var needle = require('needle');
+
 var builder = require('botbuilder');
 
 var connector = new builder.ChatConnector({
@@ -14,9 +22,25 @@ function listen() {
     };
 }
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+bot.dialog('/', function(session) {
+	if (hasImageAttachment(session)) {
+        var stream = getImageStreamFromMessage(session.message);
+        captionService
+            .getCaptionFromStream(stream)
+            .then(function (caption) { handleSuccessResponse(session, caption); })
+            .catch(function (error) { handleErrorResponse(session, error); });
+    } else {
+        var imageUrl = parseAnchorTag(session.message.text) || (validUrl.isUri(session.message.text) ? session.message.text : null);
+        if (imageUrl) {
+            captionService
+                .getCaptionFromUrl(imageUrl)
+                .then(function (caption) { handleSuccessResponse(session, caption); })
+                .catch(function (error) { handleErrorResponse(session, error); });
+        } else {
+            session.send('Did you upload an image? I\'m more of a visual person. Try sending me an image or an image URL');
+        }
+    }
+})
 
 
 // Enable Conversation Data persistence
@@ -44,3 +68,69 @@ module.exports = {
     beginDialog: beginDialog,
     sendMessage: sendMessage*/
 };
+
+
+//=========================================================
+// Utilities
+//=========================================================
+function hasImageAttachment(session) {
+    return session.message.attachments.length > 0 &&
+        session.message.attachments[0].contentType.indexOf('image') !== -1;
+}
+
+function getImageStreamFromMessage(message) {
+    var headers = {};
+    var attachment = message.attachments[0];
+    if (checkRequiresToken(message)) {
+        // The Skype attachment URLs are secured by JwtToken,
+        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+        // https://github.com/Microsoft/BotBuilder/issues/662
+        connector.getAccessToken(function (error, token) {
+            var tok = token;
+            headers['Authorization'] = 'Bearer ' + token;
+            headers['Content-Type'] = 'application/octet-stream';
+
+            return needle.get(attachment.contentUrl, { headers: headers });
+        });
+    }
+
+    headers['Content-Type'] = attachment.contentType;
+    return needle.get(attachment.contentUrl, { headers: headers });
+}
+
+function checkRequiresToken(message) {
+    return message.source === 'skype' || message.source === 'msteams';
+}
+
+/**
+ * Gets the href value in an anchor element.
+ * Skype transforms raw urls to html. Here we extract the href value from the url
+ * @param {string} input Anchor Tag
+ * @return {string} Url matched or null
+ */
+function parseAnchorTag(input) {
+    var match = input.match('^<a href=\"([^\"]*)\">[^<]*</a>$');
+    if (match && match[1]) {
+        return match[1];
+    }
+
+    return null;
+}
+
+//=========================================================
+// Response Handling
+//=========================================================
+function handleSuccessResponse(session, caption) {
+    if (caption) {
+        session.send('I think it\'s ' + caption);
+    }
+    else {
+        session.send('Couldn\'t find a caption for this one');
+    }
+
+}
+
+function handleErrorResponse(session, error) {
+    session.send('Oops! Something went wrong. Try again later.');
+    console.error(error);
+}
