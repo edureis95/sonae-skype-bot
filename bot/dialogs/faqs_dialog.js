d53cedb68c14e65f14b16119c0e9e26d350df6fd
up201305618 <up201305618@fe.up.pt>
2017-04-02 16:40:05 +0100

    faqs working using PO account

---
+++ bot/dialogs/faqs_dialog.js
@@ -0,0 +1,29 @@
+
+/*
+ * This file contains all the dialogs 
+ * related to FAQs
+ */
+
+var builder = require('botbuilder');
+var cognitiveServices = require('botbuilder-cognitiveservices'); 
+var lib = new builder.Library('faqs');
+
+// General bot responses
+var negativeResponse = 'Lamento, mas não sei nada sobre isso. Pergunta-me outras coisas.';
+
+// SONAE FAQs dialog
+var recognizer = new cognitiveServices.QnAMakerRecognizer({
+    knowledgeBaseId: process.env.SONAE_FAQS_KBID,
+    subscriptionKey: process.env.SONAE_FAQS_SUBKEY
+});
+
+lib.dialog('sonae-faqs', new cognitiveServices.QnAMakerDialog({
+    recognizers: [recognizer],
+    defaultMessage: negativeResponse,
+    qnaThreshold: 0.3
+}));
+
+// Export the dialogs
+module.exports.createLibrary = function() {
+    return lib.clone();
+};
--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,4 @@
 var builder = require('botbuilder');
-var cognitiveServices = require('botbuilder-cognitiveservices');
 
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -15,41 +14,12 @@ function listen() {
     };
 }
 
-
-//____________________________________________________________
-
-var recognizer = new cognitiveServices.QnAMakerRecognizer({
-    knowledgeBaseId: process.env.SONAE_FAQS_KBID,
-    subscriptionKey: process.env.SONAE_FAQS_SUBKEY
+bot.dialog('/', function(session) {
+    return session.beginDialog('faqs:sonae-faqs')
 });
 
-bot.dialog('/', new cognitiveServices.QnAMakerDialog({
-    recognizers: [recognizer],
-    defaultMessage: "I didn't unsertand what you say. Repeat please.",
-    qnaThreshold: 0.3
-}));
-
-//____________________________________________________________
-
-// Enable Conversation Data persistence
-//bot.set('persistConversationData', true);
-
-// Sub-Dialogs
-/*
-bot.library(require('./dialogs/...').createLibrary());
-bot.library(require('./dialogs/...').createLibrary());    
-*/
-
-/*
-function beginDialog(address, dialogId, dialogArgs) {
-    bot.beginDialog(address, dialogId, dialogArgs);
-}
-
-function sendMessage(message) {
-    bot.send(message);
-}*/
-
-
+// import faqs dialogs
+bot.library(require('./dialogs/faqs_dialog').createLibrary());
 
 module.exports = {
     listen: listen/*,
