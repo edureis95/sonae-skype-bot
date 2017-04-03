ce6ce3a04888826cbaa1a68c054d4c14f7519be5
nuno-silva18 <ei12187@fe.up.pt>
2017-04-03 22:52:59 +0100

    Removed API key (unnecessary). Added comments on how to get API to return in PT.

--- bot/dialogs/global_purpose.js
+++ bot/dialogs/global_purpose.js
@@ -1,8 +1,15 @@
+/*
+ * In order to retrive from the Bing Maps API, it's necessary to get a key (through the Bing Maps Portal) and then set it as an environmental variable in the sonae-bot dir.
+ * To do so, just open a command prompt in the dir and type: set BING_MAPS_API_KEY=<Your API key>.
+ * 
+ * In order for the API to return in pt-PT, it's necessary to modify the 'bing' NPM module. At the end of the URL concatenation functions in the module, concatenate '&culture=pt-PT'.
+ * This will allow the the API to return in pt-PT.
+ * 
+ */
+
 var builder = require('botbuilder');
 var bing = require('bing');
 var lib = new builder.Library('global_purpose');
-
-process.env['BING_MAPS_API_KEY'] = 'AqNgdqQRbHbTI1C4i8R1drdqiKJ5u9bDgeJr1HxvdghjFewRCPln1YUzORPEYdf-';
 
 lib.dialog('directions', [
     function (session, args) {
@@ -22,7 +29,7 @@ lib.dialog('directions', [
         builder.Prompts.text(session, "Qual é o modo de transporte a utilizar na viagem?");
     },
     function(session, results) {
-        //TODO: API returns results in ENG (must be PT). Limit the distance when transportation mode is walking.
+        //TODO: Limit the distance when transportation mode is walking.
 
         if(results.response == 'caminhar')
         {
