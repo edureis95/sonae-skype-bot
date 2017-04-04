0b0a2fa35241338148358e53da61b27210db63b8
Miguel Botelho <m_botelho8@hotmail.com>
2017-04-04 13:12:17 +0100

    test done but it doesn't run. tested on app.js and it was working. missing the forecast for a specific day

--- bot/services/meteo.js
+++ bot/services/meteo.js
@@ -3,37 +3,43 @@ const request = require('request-promise');
 const appId = '612180c7acafaf536644f77c8b57f2ec';
 
 /**
- * Converts Fahrenheit degrees to Celsius.
- * @param {*} f fahrenheit degrees
- * @return {*} celsius
+ * Converts Kelvin degrees to Celsius.
+ * @param {Number} f kelvin degrees
+ * @return {Number} celsius celsius degrees
  */
 function toCelsius(f) {
-    return (5 / 9) * (f - 32);
+    return f - 273.15;
 }
 
 /**
  * Call Open Weather API to request the weather for today on a specific city of a country.
- * @param {*} city name of the city 
- * @param {*} country this is the code of the country
- * @return 
+ * This API is kind of tricky. It always sends a response back, even if the name is completely wrong, like a random character. Be aware of that.
+ * @param {String} city name of the city 
+ * @param {String} country code of the country (ex: pt, en, us, ...)
+ * @param {Function} callback function with only one parameter, in which goes the JSON response
  */
-function getWeatherByCityToday(city, country) {
+function getWeatherByCityToday(city, country, next) {
     request('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&appid=' + appId)
         .then(function (body) {
             const bodyParsed = JSON.parse(body);
-            let respo = {
-                description,
-                main,
-                min_temperature,
-                max_temperature,
-                humidity,
-                wind_velocity
-            };
             // send bodyParsed to respo var. see the documentation on the docs of open weather.
+            
+            if (bodyParsed.sys.country != country)
+                next('Code given doesn\'t match the Open Weather API. Given: ' + country + ', Retrieved: ' + bodyParsed.sys.country);
+
+            let respo = {
+                description : bodyParsed.weather[0].description,
+                short_description : bodyParsed.weather[0].main,
+                humidity : bodyParsed.main.humidity + '%',
+                wind_velocity : bodyParsed.wind.speed + 'meters/second',
+                temperature : toCelsius(bodyParsed.main.temp),
+                clouds : bodyParsed.clouds.all + '%',
+            }
+            next(respo);
         })
         .catch(function (err) {
-
+            next('Error calling OpenWeatherMap API. Error: ' + err.message);
         });
 }
 
-module.exports = {};
+module.exports = {getWeatherByCityToday};
--- bot/tests/test.js
+++ bot/tests/test.js
@@ -2,7 +2,8 @@
  * Created by Miguel Botelho on 31/03/2017.
  * To run this module, one only needs to run 'npm test' on the command line.
  */
-var assert = require('assert');
+const assert = require('assert');
+const meteo = require('../services/meteo.js');
 
 /**
  * This is only a test example.
@@ -12,6 +13,20 @@ describe('Array', function () {
     describe('#indexOf()', function () {
         it('should return -1 when the value is not present', function () {
             assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+    });
+});
+
+
+/**
+ * 
+ */
+describe('Tests the connection to OpenWeatherMap API', function () {
+    this.timeout(15000);
+    it('should return porto', function () {
+        meteo.getWeatherByCityToday('porto', 'pt', function (porto_meteo) {
+            console.log(porto_meteo);
+            assert.equal(porto_meteo.name, 'porto');
         });
     });
 });
