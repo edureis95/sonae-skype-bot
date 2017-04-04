6b259a2be283c1efd234db434e410ce19ce7833a
Miguel Botelho <m_botelho8@hotmail.com>
2017-04-04 15:55:11 +0100

    tests added. meteorology finished.

--- bot/services/meteo.js
+++ bot/services/meteo.js
@@ -12,8 +12,25 @@ function toCelsius(f) {
 }
 
 /**
+ * Converts an UNIX TimeStamp to a date(day of the month) and an hour.
+ * @param {UNIX_Timestamp} UNIX_timestamp  
+ * @return {Object} time contains the hour and date
+ */
+function timeConverter(UNIX_timestamp) {
+    const a = new Date(UNIX_timestamp * 1000);
+    const date = a.getDate();
+    const hour = a.getHours() - 1;
+    let time = {
+        date,
+        hour,
+    }
+    return time;
+}
+
+/**
  * Call Open Weather API to request the weather for today on a specific city of a country.
  * This API is kind of tricky. It always sends a response back, even if the name is completely wrong, like a random character. Be aware of that.
+ * It returns a JSON Object with a detailed description, a short one, humidity, wind_velocity, temperature and percentage of clouds.
  * @param {String} city name of the city 
  * @param {String} country code of the country (ex: pt, en, us, ...)
  * @param {Function} callback function with only one parameter, in which goes the JSON response
@@ -22,18 +39,20 @@ function getWeatherByCityToday(city, country, next) {
     request('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&appid=' + appId)
         .then(function (body) {
             const bodyParsed = JSON.parse(body);
-            // send bodyParsed to respo var. see the documentation on the docs of open weather.
-            
-            if (bodyParsed.sys.country != country)
+
+            if (bodyParsed.sys.country.toUpperCase() != country.toUpperCase())
                 next('Code given doesn\'t match the Open Weather API. Given: ' + country + ', Retrieved: ' + bodyParsed.sys.country);
 
+            // this is the response object
+            // FIXME: the descriptions are in english
             let respo = {
-                description : bodyParsed.weather[0].description,
-                short_description : bodyParsed.weather[0].main,
-                humidity : bodyParsed.main.humidity + '%',
-                wind_velocity : bodyParsed.wind.speed + 'meters/second',
-                temperature : toCelsius(bodyParsed.main.temp),
-                clouds : bodyParsed.clouds.all + '%',
+                description: bodyParsed.weather[0].description,
+                short_description: bodyParsed.weather[0].main,
+                humidity: bodyParsed.main.humidity + '%',
+                wind_velocity: bodyParsed.wind.speed + ' metros/segundo',
+                temperature: toCelsius(bodyParsed.main.temp),
+                clouds: bodyParsed.clouds.all + '%',
+                country : bodyParsed.name,
             }
             next(respo);
         })
@@ -42,4 +61,101 @@ function getWeatherByCityToday(city, country, next) {
         });
 }
 
-module.exports = {getWeatherByCityToday};
+/**
+ * Call Open Weather API to request the weather for today on a specific city of a country.
+ * This API is kind of tricky. It always sends a response back, even if the name is completely wrong, like a random character. Be aware of that.
+ * It returns a JSON Object with 3 objects inside it, morning, afternoon and evening, which one  with a detailed description, a short one, humidity, wind_velocity, temperature and percentage of clouds.
+ * @param {String} city name of the city 
+ * @param {String} country code of the country (ex: pt, en, us, ...)
+ * @param {Number} day in which 0 is for today, 1 for tomorrow up until 4
+ * @param {Function} callback function with only one parameter, in which goes the JSON response
+ */
+function getWeatherByCityOnDay(city, country, day, next) {
+    request('http://api.openweathermap.org/data/2.5/forecast?q=' + city + ',' + country + '&appid=' + appId)
+        .then(function (body) {
+            const bodyParsed = JSON.parse(body);
+
+            if (bodyParsed.city.country.toUpperCase() != country.toUpperCase())
+                next('Code given doesn\'t match the Open Weather API. Given: ' + country + ', Retrieved: ' + bodyParsed.city.country);
+
+            const dayToday = (new Date()).getDate();
+            var reachedTime = false;
+
+            // this is the response object
+            var respo = {
+                // 6 am to 12 pm
+                morning: {
+                    description: undefined,
+                    short_description: undefined,
+                    humidity: undefined,
+                    wind_velocity: undefined,
+                    temperature: undefined,
+                    clouds: undefined,
+                },
+                // 12 pm to 6pm
+                afternoon: {
+                    description: undefined,
+                    short_description: undefined,
+                    humidity: undefined,
+                    wind_velocity: undefined,
+                    temperature: undefined,
+                    clouds: undefined,
+                },
+                // 6pm to 12am
+                evening: {
+                    description: undefined,
+                    short_description: undefined,
+                    humidity: undefined,
+                    wind_velocity: undefined,
+                    temperature: undefined,
+                    clouds: undefined,
+                },
+                country : bodyParsed.city.name,
+            }
+
+            // parses only the relevant information. it runs through a list and adds to the correct day, according to morning, afternoon and evening.
+            // FIXME: the descriptions are in english
+            for (let i = 0; i < bodyParsed.list.length; i++) {
+                var time = timeConverter(bodyParsed.list[i].dt);
+                if ((time.date - dayToday) === day) {
+                    reachedTime = true;
+                    // morning
+                    if (time.hour === 9) {
+                        respo.morning.description = bodyParsed.list[i].weather[0].description;
+                        respo.morning.short_description = bodyParsed.list[i].weather[0].main;
+                        respo.morning.humidity = bodyParsed.list[i].main.humidity + '%';
+                        respo.morning.wind_velocity = bodyParsed.list[i].wind.speed + ' metros/segundo';
+                        respo.morning.temperature = toCelsius(bodyParsed.list[i].main.temp);
+                        respo.morning.clouds = bodyParsed.list[i].clouds.all + '%';
+                    }
+                    // afternoon
+                    else if (time.hour === 15) {
+                        respo.afternoon.description = bodyParsed.list[i].weather[0].description;
+                        respo.afternoon.short_description = bodyParsed.list[i].weather[0].main;
+                        respo.afternoon.humidity = bodyParsed.list[i].main.humidity + '%';
+                        respo.afternoon.wind_velocity = bodyParsed.list[i].wind.speed + ' metros/segundo';
+                        respo.afternoon.temperature = toCelsius(bodyParsed.list[i].main.temp);
+                        respo.afternoon.clouds = bodyParsed.list[i].clouds.all + '%';
+                    }
+                    // evening
+                    else if (time.hour === 21) {
+                        respo.evening.description = bodyParsed.list[i].weather[0].description;
+                        respo.evening.short_description = bodyParsed.list[i].weather[0].main;
+                        respo.evening.humidity = bodyParsed.list[i].main.humidity + '%';
+                        respo.evening.wind_velocity = bodyParsed.list[i].wind.speed + ' metros/segundo';
+                        respo.evening.temperature = toCelsius(bodyParsed.list[i].main.temp);
+                        respo.evening.clouds = bodyParsed.list[i].clouds.all + '%';
+                    }
+                    // don't waste unnecessary time
+                } else if (reachedTime) {
+                    i = bodyParsed.list.length;
+                }
+            }
+            next(respo);
+        })
+        .catch(function (err) {
+            next('Error calling OpenWeatherMap API. Error: ' + err.message);
+        });
+}
+
+module.exports = { getWeatherByCityToday, getWeatherByCityOnDay };
--- bot/tests/test.js
+++ bot/tests/test.js
@@ -11,22 +11,62 @@ const meteo = require('../services/meteo.js');
 describe('Array', function () {
     this.timeout(15000);
     describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
+        it('should return -1 when the value is not present', function (done) {
             assert.equal(-1, [1, 2, 3].indexOf(4));
+            done();
         });
     });
 });
 
 
 /**
- * 
+ * Gets the weather for today, on Porto, Portugal.
  */
 describe('Tests the connection to OpenWeatherMap API', function () {
     this.timeout(15000);
-    it('should return porto', function () {
-        meteo.getWeatherByCityToday('porto', 'pt', function (porto_meteo) {
-            console.log(porto_meteo);
-            assert.equal(porto_meteo.name, 'porto');
+    it('should return porto', function (done) {
+        meteo.getWeatherByCityToday('porto', 'pt', function (weather) {
+            assert.equal(weather.country.toLowerCase(), 'porto');
+            done();
+        });
+    });
+});
+
+/**
+ * Gets the weather for a specific day, for Valencia, Spain.
+ */
+describe('Tests the connection to OpenWeatherMap API', function () {
+    this.timeout(15000);
+    it('should return the wrong country', function (done) {
+        meteo.getWeatherByCityToday('din12iesainkasdni', 'es', function (weather) {
+            assert.notEqual(weather.country, 'din12iesainkasdni');
+            done();
+        });
+    });
+});
+
+/**
+ * Gets the weather for a specific day, for Valencia, Spain.
+ */
+describe('Tests the connection to OpenWeatherMap API', function () {
+    this.timeout(15000);
+    it('should return valencia', function (done) {
+        meteo.getWeatherByCityOnDay('valencia', 'es', 2, function (weather) {
+            assert.equal(weather.country.toLowerCase(), 'valencia');
+            done();
+        });
+    });
+});
+
+/**
+ * Gets the weather for a specific day, for Valencia, Spain.
+ */
+describe('Tests the connection to OpenWeatherMap API', function () {
+    this.timeout(15000);
+    it('should return the wrong country', function (done) {
+        meteo.getWeatherByCityOnDay('din12iesainkasdni', 'es', 2, function (weather) {
+            assert.notEqual(weather.country, 'din12iesainkasdni');
+            done();
         });
     });
 });
