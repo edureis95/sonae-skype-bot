ba29f40f384dcb8b093638111b54f059cf852326
edureis95 <up201303832@gcloud.fe.up.pt>
2017-04-07 20:26:27 +0100

    Tests implemented

--- bot/dialogs/global_purpose.js
+++ bot/dialogs/global_purpose.js
@@ -13,20 +13,17 @@ lib.dialog('analyze_image', function (session) {
     
     var attachment = session.message.attachments[0];
 
-    download(attachment.contentUrl, 'uploads/temp.' + attachment.contentType.substring(6), function() {
-        // The path to the local image file, e.g. "/path/to/image.png"
-        const fileName = 'uploads/temp.' + attachment.contentType.substring(6);
+    var date = new Date();
+    var t = date.getTime();
 
-        // Detect similar images on the web to a local file
-        vision.detectSimilar(fileName)
-        .then((data) => {
-            const results = data[1].responses[0].webDetection;
+    const fileName = 'uploads/temp' + t + '.' + attachment.contentType.substring(6);
 
-            if (results.webEntities.length > 0) {
-                session.endDialog(results.webEntities[0].description);
-            }
-
-            fs.unlink('uploads/temp.' + attachment.contentType.substring(6));
+    //Save temp image file
+    download(attachment.contentUrl, fileName, function() {
+        checkImage(fileName, function(caption) {
+            session.endDialog(caption);
+            //Delete temp image file
+            fs.unlink(fileName);
         });
     });
 });
@@ -39,6 +36,12 @@ lib.dialog('food_menu', function(session) {
 // Utilities
 //=========================================================
 
+/**
+ * Download file from url
+ * @param {*} uri 
+ * @param {*} filename 
+ * @param {*} callback 
+ */
 var download = function(uri, filename, callback){
   request.head(uri, function(err, res, body){
     console.log('content-type:', res.headers['content-type']);
@@ -48,7 +51,27 @@ var download = function(uri, filename, callback){
   });
 };
 
+/**
+ * Use vision API to get caption from image file
+ * @param {*} fileName 
+ * @param {*} callback 
+ */
+function checkImage(fileName, callback) {
+    vision.detectSimilar(fileName)
+    .then((data) => {
+        const results = data[1].responses[0].webDetection;
+
+        if (results.webEntities.length > 0) {
+            callback(results.webEntities[0].description);
+        }
+        else
+            callback('Couldn\'t find anything');
+    });
+}
+
 // Export createLibrary() function
 module.exports.createLibrary = function () {
     return lib.clone();
 };
+
+module.exports = { checkImage };
--- bot/tests/test.js
+++ bot/tests/test.js
@@ -3,30 +3,23 @@
  * To run this module, one only needs to run 'npm test' on the command line.
  */
 const assert = require('assert');
-const captionService = require('../services/caption-service');
-const needle = require('needle');
+const global_purpose = require('../dialogs/global_purpose.js')
 
 /**
- * Gets the caption from a specific image using the Vision API.
+ * Gets the caption from a specific image using the Google Vision API.
  */
-describe('Tests the connection to the Vision API', function () {
+describe('Tests the connection to the Google Vision API', function () {
     this.timeout(15000);
-    it('should return Lebron James', function (done) {
-        var headers = {};
-        headers['Content-Type'] = 'image/png';
-        var stream = needle.get('http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png&w=350&h=254', { headers: headers });
-        captionService.getCaptionFromStream(stream).then(function (caption) {
-            assert.equal(caption, 'LeBron James that is posing for a picture');
+    it('should return Beagle', function (done) {
+        global_purpose.checkImage('bot/tests/Beagle.JPG', function(caption) {
+            assert.equal(caption, 'Beagle');
             done();
         });
     });
  
-    it('should return Cristiano Ronaldo', function (done) {
-        var headers = {};
-        headers['Content-Type'] = 'image/png';
-        var stream = needle.get('https://s-media-cache-ak0.pinimg.com/originals/50/d4/a3/50d4a3ba96253b15febc42caf163e072.jpg', { headers: headers });
-        captionService.getCaptionFromStream(stream).then(function (caption) {
-            assert.equal(caption, 'Cristiano Ronaldo is smiling at the camera');
+    it('should return Jade Belt Bridge', function (done) {
+        global_purpose.checkImage('bot/tests/Bridge.jpg', function(caption) {
+            assert.equal(caption, 'Jade Belt Bridge');
             done();
         });
     });
