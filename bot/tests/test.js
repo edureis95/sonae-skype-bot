dbeb692672a91cfa410519f5bf066ac64bf26d0a
edureis95 <up201303832@gcloud.fe.up.pt>
2017-04-14 00:30:04 +0100

    Tests done

--- bot/tests/test.js
+++ bot/tests/test.js
@@ -2,7 +2,9 @@
  * Created by Miguel Botelho on 31/03/2017.
  * To run this module, one only needs to run 'npm test' on the command line.
  */
+require('dotenv-extended').load();
 const assert = require('assert');
+const sharepoint = require("../services/sharepoint/sharepoint");
 
 /**
  * This is only a test example.
@@ -13,5 +15,22 @@ describe('Array', function () {
     it('should return -1 when the value is not present', () => {
       assert.equal(-1, [1, 2, 3].indexOf(4));
     });
+  });
+});
+
+
+/**
+ * Test retrievel of food menu URL from sharepoint
+ */
+describe('Test food menu', function () {
+  this.timeout(15000);
+  it('should return food menu url', function (done) {
+      //todo alterar para o nome do ficheiro pretendido conforme
+      let filename = "'exemplo.pdf'";
+      sharepoint.getFileUrlFromSharePoint(filename, function (url) {
+          //todo alterar url
+          assert.equal(url, 'https://sonaesystems.sharepoint.com/sites/SonaeBot/Shared Documents/exemplo.pdf');
+          done();
+      });
   });
 });
